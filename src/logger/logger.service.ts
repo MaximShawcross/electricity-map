6376685f8a0abac10831d3fc0d527b58e6469b19
Kushnir Maxim <maxbasta2006@gmail.com>
2022-11-30 20:14:58 +0100

    logger service

--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -1,1 +1,29 @@
+import { Logger } from "tslog";
 
+import { ILoggerService } from "./logger.interface";
+
+export default class LoggerService implements ILoggerService {
+	private logger: Logger;
+	
+	constructor() {
+		this.logger = new Logger({
+			displayInstanceName: false,
+			displayLoggerName: false,
+			displayFilePath: "hidden",
+			displayFunctionName: false
+		});
+	}
+
+	info(...args: unknown[]): void {
+		this.logger.info(...args);
+	};
+
+	error(...args: unknown[]): void{
+		this.logger.error(...args);
+	};
+
+	warn(...args: unknown[]): void{
+		this.logger.warn(...args);
+	};
+	
+}
